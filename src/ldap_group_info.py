61c1d8175526ab9320dd9e53bd284fa61bac47f0
Andy Loftus <aloftus@gmail.com>
2023-07-14 03:32:25 +0000

    get member list

--- src/ldap_group_info.py
+++ src/ldap_group_info.py
@@ -1,30 +1,18 @@
 import ldap3
 
 ldap_server = "ldaps://ldap1.ncsa.illinois.edu"  # Replace with your LDAP server
-#ldap_user = "cn=admin,dc=example,dc=com"  # Replace with your LDAP admin user
-#ldap_password = "your-password"  # Replace with your LDAP admin user's password
+# ldap_user = "cn=admin,dc=example,dc=com"  # Replace with your LDAP admin user
+# ldap_password = "your-password"  # Replace with your LDAP admin user's password
 ldap_user = None
 ldap_password = None
 
 search_base = 'dc=ncsa,dc=illinois,dc=edu'
 search_scope = ldap3.SUBTREE
-#attributes = ldap3.ALL_ATTRIBUTES
-attributes = ["description", "gidNumber", "businessCategory", "owner", "member"]
+attributes = ldap3.ALL_ATTRIBUTES
+# attributes = ["description", "gidNumber", "businessCategory", "owner", "member"]
 
 group_list = [
-    'caiiwp ',
-    'grp_aifarms_wp ',
-    'grp_caps ',
-    'grp_ceesd_wp ',
-    'grp_digi_mat ',
-    'grp_reu_intl ',
-    'grp_web_delta ',
-    'grp_web_digitalag ',
-    'grp_web_gravity ',
-    'grp_web_iccp ',
-    'grp_web_lci ',
-    'grp_web_reu ',
-    'grp_web_ssa ',
+    'org_ici ',
 ]
 
 with ldap3.Connection(ldap_server, ldap_user, ldap_password) as conn:
@@ -40,7 +28,8 @@ with ldap3.Connection(ldap_server, ldap_user, ldap_password) as conn:
                 entry = conn.entries[0]
                 print(f"Group: {group_name}")
                 print(f"Description: {entry.description}")
-                # print(f"GID number: {entry.gidNumber}")
-                # print(f"Business category: {entry.businessCategory}")
+                print(f"GID number: {entry.gidNumber}")
+                print(f"Business category: {entry.businessCategory}")
                 print(f"Owner: {entry.owner}")
-                print(f"Members: {entry.member}")
+                members = [ m.split(',')[0].split('=')[1] for m in entry.uniqueMember ]
+                print(f"Members: {members}")
